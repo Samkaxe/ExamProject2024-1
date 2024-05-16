97c25f74ad84ee0cd922bbc734ae4692df47c0ba
Renaldinho <rena0968@easv365.dk>
2024-05-16 10:28:00 +0200

    .

--- AuthService/Auth.Infrastructure/DependencyResolvement/DependencyResolverService.cs
+++ AuthService/Auth.Infrastructure/DependencyResolvement/DependencyResolverService.cs
@@ -11,7 +11,10 @@ public static class DependencyResolverService
     public static void RegisterInfrastructureLayer(IServiceCollection services)
     {
         services.AddScoped<ICredentialRepository, CredentialRepository>();
-        services.AddDbContext<DatabaseContext>(options =>
-            options.UseSqlite("Data Source=auth.db"));
+        // services.AddDbContext<DatabaseContext>(options =>
+        //     options.UseSqlite("Data Source=auth.db"));
+        
+        services.AddDbContext<DatabaseContext>(options => 
+            options.UseNpgsql("Host=auth-sql;Database=auth_database;Username=user;Password=pass"));
     }
 }
