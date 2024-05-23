e3dabca2195bdd3e416bf2728980d9999dcc5365
Samkaxe <must2765@easv365.dk>
2024-05-23 19:16:00 +0200

    adding additonal method to redis

---
+++ Checkoutservice/Controllers/CheckoutController.cs
@@ -0,0 +1,34 @@
+﻿using Checkoutservice.Extintions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checkoutservice.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CheckoutController : ControllerBase
+{
+    private readonly RedisCacheService _redisCacheService;
+
+    public CheckoutController(RedisCacheService redisCacheService)
+    {
+        _redisCacheService = redisCacheService;
+    }
+
+    [HttpPost("start")]
+    public IActionResult StartCheckoutSession(string userId, string orderId)
+    {
+        _redisCacheService.SetCheckoutSession(userId, orderId);
+        return Ok($"Checkout session started for User: {userId} with Order: {orderId}");
+    }
+
+    [HttpGet("session")]
+    public IActionResult GetCheckoutSession(string userId)
+    {
+        var orderId = _redisCacheService.GetCheckoutOrderId(userId);
+        if (orderId != null)
+        {
+            return Ok($"User: {userId} has an active order: {orderId}");
+        }
+        return NotFound("No active checkout session found for this user.");
+    }
+}
