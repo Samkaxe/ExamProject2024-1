93c6a23f17ba3e02c955641bf6814a4b24c773d0
Samkaxe <must2765@easv365.dk>
2024-05-28 19:34:19 +0200

    sd

--- Inventoryservice/Controllers/ProductController.cs
+++ Inventoryservice/Controllers/ProductController.cs
@@ -3,98 +3,156 @@ using Core.Dtos;
 using Core.Entites;
 using InventoryService.Extintions;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Driver;
+using Microsoft.Extensions.Logging;
 
 namespace InventoryService.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class ProductController : ControllerBase
-{
-    private readonly IMapper _mapper;
-    private readonly MongoService _mongoService;
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly MongoService _mongoService;
+        private readonly ILogger<ProductController> _logger;
 
-    public ProductController(IMapper mapper, MongoService mongoService)
-    {
-        _mapper = mapper;
-        _mongoService = mongoService;
-    }
-    
-    [HttpGet("CheckConnection")]
-    public IActionResult CheckConnection()
-    {
-        if (_mongoService.IsConnectionEstablished())
+        public ProductController(IMapper mapper, MongoService mongoService, ILogger<ProductController> logger)
         {
-            return Ok("Connection to MongoDB is established.");
-        }
-        else
-        {
-            return Problem("Unable to establish connection to MongoDB.");
-        }
-    }
-    
-    [HttpGet("brands")]
-    public IActionResult GetAllBrands()
-    {
-        var brands = _mongoService.Brands.Find(_ => true).ToList();
-        var brandDtos = _mapper.Map<List<BrandDto>>(brands);
-        return Ok(brandDtos);
-    }
-    
-    [HttpPost("brands")]
-    public IActionResult CreateBrand([FromBody] BrandDto brandDto)
-    {
-        var brand = _mapper.Map<Brand>(brandDto);
-        _mongoService.Brands.InsertOne(brand);
-        var brandDtos = _mapper.Map<BrandDto>(brand);
-        return CreatedAtAction(nameof(GetAllBrands), new { id = brandDtos.Id }, brandDto);
-    }
-
-    [HttpPost("products")]
-    public IActionResult CreateProduct([FromBody] CreateProductDto productDto)
-    {
-        var product = _mapper.Map<Product>(productDto);
-        _mongoService.Products.InsertOne(product);
-        var productDtos = _mapper.Map<ProductDto>(product);
-        return CreatedAtAction(nameof(GetAllProducts), new { id = productDtos.Id }, productDto);
-    }
-
-    [HttpGet("products")]
-    public ActionResult<List<ProductDto>> GetAllProducts()
-    {
-        var products = _mongoService.Products.Find(product => true).ToList();
-        var productDtos = new List<ProductDto>();
-
-        foreach (var product in products)
-        {
-            var brand = _mongoService.Brands.Find(b => b.Id == product.BrandId).FirstOrDefault();
-            var category = _mongoService.Categories.Find(c => c.Id == product.CategoryId).FirstOrDefault();
-
-            var productDto = _mapper.Map<ProductDto>(product);
-            productDto.Brand = _mapper.Map<BrandDto>(brand);
-            productDto.Category = _mapper.Map<CategoryDto>(category);
-
-            productDtos.Add(productDto);
+            _mapper = mapper;
+            _mongoService = mongoService;
+            _logger = logger;
         }
 
-        return productDtos;
-    }
-    
-    [HttpGet("categories")]
-    public IActionResult GetAllCategories()
-    {
-        var categories = _mongoService.Categories.Find(_ => true).ToList();
-        var categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
-        return Ok(categoryDtos);
+        [HttpGet("CheckConnection")]
+        public IActionResult CheckConnection()
+        {
+            try
+            {
+                if (_mongoService.IsConnectionEstablished())
+                {
+                    return Ok("Connection to MongoDB is established.");
+                }
+                else
+                {
+                    return Problem("Unable to establish connection to MongoDB.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while checking the MongoDB connection.");
+                return Problem("An error occurred while checking the MongoDB connection.");
+            }
+        }
+
+        [HttpGet("brands")]
+        public IActionResult GetAllBrands()
+        {
+            try
+            {
+                var brands = _mongoService.GetAllBrands();
+                var brandDtos = _mapper.Map<List<BrandDto>>(brands);
+                return Ok(brandDtos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving brands.");
+                return Problem("An error occurred while retrieving brands.");
+            }
+        }
+
+        [HttpPost("brands")]
+        public IActionResult CreateBrand([FromBody] BrandDto brandDto)
+        {
+            try
+            {
+                var brand = _mapper.Map<Brand>(brandDto);
+                _mongoService.InsertBrand(brand);
+                var brandDtos = _mapper.Map<BrandDto>(brand);
+                return CreatedAtAction(nameof(GetAllBrands), new { id = brandDtos.Id }, brandDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a brand.");
+                return Problem("An error occurred while creating a brand.");
+            }
+        }
+
+        [HttpPost("products")]
+        public IActionResult CreateProduct([FromBody] CreateProductDto productDto)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(productDto);
+                _mongoService.InsertProduct(product);
+                var productDtos = _mapper.Map<ProductDto>(product);
+                return CreatedAtAction(nameof(GetAllProducts), new { id = productDtos.Id }, productDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a product.");
+                return Problem("An error occurred while creating a product.");
+            }
+        }
+
+        [HttpGet("products")]
+        public ActionResult<List<ProductDto>> GetAllProducts()
+        {
+            try
+            {
+                var products = _mongoService.GetAllProducts();
+                var productDtos = new List<ProductDto>();
+
+                foreach (var product in products)
+                {
+                    var brand = _mongoService.GetBrandById(product.BrandId);
+                    var category = _mongoService.GetCategoryById(product.CategoryId);
+
+                    var productDto = _mapper.Map<ProductDto>(product);
+                    productDto.Brand = _mapper.Map<BrandDto>(brand);
+                    productDto.Category = _mapper.Map<CategoryDto>(category);
+
+                    productDtos.Add(productDto);
+                }
+
+                return productDtos;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving products.");
+                return Problem("An error occurred while retrieving products.");
+            }
+        }
+
+        [HttpGet("categories")]
+        public IActionResult GetAllCategories()
+        {
+            try
+            {
+                var categories = _mongoService.GetAllCategories();
+                var categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
+                return Ok(categoryDtos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving categories.");
+                return Problem("An error occurred while retrieving categories.");
+            }
+        }
+
+        [HttpPost("categories")]
+        public IActionResult CreateCategory([FromBody] CategoryDto categoryDto)
+        {
+            try
+            {
+                var category = _mapper.Map<Category>(categoryDto);
+                _mongoService.InsertCategory(category);
+                var categoryDtos = _mapper.Map<CategoryDto>(category);
+                return CreatedAtAction(nameof(GetAllCategories), new { id = categoryDtos.Id }, categoryDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a category.");
+                return Problem("An error occurred while creating a category.");
+            }
+        }
     }
 
-    [HttpPost("categories")]
-    public IActionResult CreateCategory([FromBody] CategoryDto categoryDto)
-    {
-        var category = _mapper.Map<Category>(categoryDto);
-        _mongoService.Categories.InsertOne(category);
-        var categoryDtos = _mapper.Map<CategoryDto>(category);
-        return CreatedAtAction(nameof(GetAllCategories), new { id = categoryDtos.Id }, categoryDto);
-    }
-    
-}
